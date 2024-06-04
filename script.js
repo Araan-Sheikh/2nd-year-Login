ed6cf544b7a0b795ef63c3e94496fc2f56ff7bb7
Your Name <your.email@example.com>
2024-06-04 22:58:43 +0530

    hari

--- script.js
+++ script.js
@@ -92,3 +92,38 @@ setTimeout(function() {
   document.getElementById('loading-overlay').style.display = 'none';
 }, 3000);
 
+
+
+
+
+
+
+
+
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  showInstallPromotion();
+});
+
+buttonInstall.addEventListener('click', () => {
+  // Hide the app provided install promotion
+  hideInstallPromotion();
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+    deferredPrompt = null;
+  });
+});
