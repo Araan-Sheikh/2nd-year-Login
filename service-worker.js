ff7fda44df87516d7fb443cc5cf288e8e91053f1
Your Name <your.email@example.com>
2024-06-09 09:41:37 +0530

    hari

--- service-worker.js
+++ service-worker.js
@@ -21,3 +21,16 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
+
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installing.');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activating.');
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
